78cfc918cc4d49822f119535b8c2316558ccdc1c
jiong-long <jiong_long@163.com>
2021-05-20 10:46:49 +0800

    多版本

--- consumer/src/main/java/com/jianghu/consumer/ConsumerService.java
+++ consumer/src/main/java/com/jianghu/consumer/ConsumerService.java
@@ -12,7 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsumerService {
 
-    @Reference(check = false, retries = 4, loadbalance = "LeastActive")
+    /**
+     * check：启动时是否检查，false不检查，调用时提供者不存在报错
+     * retries：重试次数，不算第一次调用后，重试的次数
+     * loadbalance：负载均衡策略，roundrobin随机
+     * version：多版本，*不区分版本
+     */
+    @Reference(check = false, retries = 4, loadbalance = "roundrobin" , version = "*")
     private ProviderService providerService;
 
     public String initDubbo(String world){
