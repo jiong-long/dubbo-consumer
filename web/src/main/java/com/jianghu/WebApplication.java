78d79dc656ecdee03151e725287294d43f2055e6
jiong-long <jiong_long@163.com>
2021-08-08 14:52:58 +0800

    rabbitmq springboot

--- web/src/main/java/com/jianghu/WebApplication.java
+++ web/src/main/java/com/jianghu/WebApplication.java
@@ -1,17 +1,35 @@
 package com.jianghu;
 
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * @author wangjinlong
  */
+@Slf4j
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-		System.out.println("http://127.0.0.1:8080/init");
+		ConfigurableApplicationContext application = SpringApplication.run(WebApplication.class, args);
+
+		//显示项目访问地址
+		Environment env = application.getEnvironment();
+		String ip = "127.0.0.1";
+		String port = env.getProperty("server.port");
+		String path = env.getProperty("server.servlet.context-path");
+		if (StrUtil.isEmpty(path)) {
+			path = "";
+		}
+		log.info("\n\t----------------------------------------------------------\n\t" +
+				"Application is running! Access URLs:\n\t" +
+				"项目访问地址: \t\thttp://" + ip + ":" + port + path + "\n\t" +
+				"Swagger访问地址: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
+				"----------------------------------------------------------");
 	}
 
 }
